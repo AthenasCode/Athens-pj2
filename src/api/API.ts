88937d723172f97d7b1f1f61f2f62f14d5737dfb
torrober <robemario124@gmail.com>
2024-09-28 18:40:18 -0500

    Create API.ts

---
+++ src/api/API.ts
@@ -0,0 +1,23 @@
+export default class API {
+    static url = "https://web-fe-html-css-athens-prj2-api.onrender.com/";
+    static async getMenuItems() {
+        const response = await fetch(`${this.url}/menuItems`);
+        return response.json();
+    }
+    static async getBreadcrumbs() {
+        const response = await fetch(`${this.url}/linksbread`);
+        return response.json();
+    }
+    static async getPLPproducts() {
+        const response = await fetch(`${this.url}/productsPLP`);
+        return response.json();
+    }
+    static async getFilterCategories() {
+        const response = await fetch(`${this.url}/filterCategories`);
+        return response.json();
+    }
+    static async getAdImage() {
+        const response = await fetch(`${this.url}/adImage`);
+        return response.json();
+    }
+}
