0913f9e2e7e9002d112bf3507161865dde76a0f2
torrober <robemario124@gmail.com>
2024-09-28 19:12:01 -0500

    api loading done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,44 @@
 import { Main } from "../layout/Main";
 import Card from "../components/Card";
 import Breadcrumb from "../components/Breadcrumb";
-import { adImage, homepageItems, linksbread } from "../utils/data";
+import { adImage,linksbread } from "../utils/data";
 import AdImage from "../components/AdImage";
 import Banner from "../components/Banner";
+import { useEffect, useState } from "react";
+import API from "../api/API";
+import { Product } from "../types/product.type";
+import spinner from "../../public/images/Loading_2.gif";
 export function Home() {
+  const [homepageItems, setHomepageItems] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    API.getHomePageItems().then((data) => {
+      setHomepageItems(data);
+      setLoading(false);
+    });
+  }, []);
   return (
     <Main>
 
       <Banner />
       <Breadcrumb links={[linksbread[0]]}/>
       <section className="container">
-
-
-
         <h2>¡Mira los mejores productos que tenemos!</h2>
         <div className="productsList">
-          {homepageItems.map((product) => (
-            <Card
-              image={"" + product.image}
-              title={"" + product.title}
-              description={"" + product.description}
-              price={product.original_price}
-              discountPrice={product.discounted_price}
-              buttonLink={"#"}
-            />
-          ))}
+          {loading ? (
+            <img src={spinner} alt="Cargando..." />
+          ) : (
+            homepageItems.map((product: Product) => (
+              <Card // Asegúrate de añadir una key única
+                image={"" + product.image}
+                title={"" + product.title}
+                description={"" + product.description}
+                price={product.original_price}
+                discountPrice={product.discounted_price}
+                buttonLink={"#"}
+              />
+            ))
+          )}
         </div>
       </section>
 
