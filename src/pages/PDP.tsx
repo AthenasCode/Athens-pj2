e761d41afad2cd5e0773b50ef9d6cc0d5d40378c
Freddy Fajardo <affajardo@uninorte.edu.co>
2024-10-04 14:29:14 -0500

    Cambios completos PDP

--- src/pages/PDP.tsx
+++ src/pages/PDP.tsx
@@ -2,54 +2,37 @@ import { useEffect, useState } from "react";
 import { Main } from "../layout/Main";
 import { linksbread } from "../utils/data";
 import Breadcrumb from "../components/Breadcrumb";
-import API from "../api/API";
+import { useGetPLPProducts } from "../hooks/useGetPLPProducts"; 
 import Error from "../components/Error";
 import ProductCard from "../components/ProductCard";
-import spinner from "../../public/images/Loading_2.gif"; // Ruta correcta del spinner
+import spinner from "../../public/images/Loading_2.gif";
 
 export function PDP() {
-  const [product, setProduct] = useState<any>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [relatedProducts, setRelatedProducts] = useState<any[]>([]);
   const params = new URLSearchParams(location.search);
   const productId = parseInt(params.get("product") || "0");
   const category = params.get("category") || "";
+  
+  const { data: products, isLoading, isError, error } = useGetPLPProducts(category);
+  
+  const [product, setProduct] = useState<any>();
+  const [relatedProducts, setRelatedProducts] = useState<any[]>([]);
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      setLoading(true);
-      try {
-        const data = await API.getPLPproducts();
-        const categoryData = data.find((cat: any) => cat.categorie === category);
-
-        if (categoryData) {
-          const foundProduct = categoryData.products.find((prod: any) => prod.id === productId);
-          if (foundProduct) {
-            setProduct(foundProduct);
-            setRelatedProducts(getRelatedProducts(categoryData.products));
-          } else {
-            setError(true);
-            setErrorMessage("Producto no encontrado");
-          }
-        } else {
-          setError(true);
-          setErrorMessage("Categoría no encontrada");
+    if (products) {
+      const categoryData = products.find((cat) => cat.categorie === category);
+      
+      if (categoryData) {
+        const foundProduct = categoryData.products.find((prod: any) => prod.id === productId);
+        if (foundProduct) {
+          setProduct(foundProduct);
+          setRelatedProducts(getRelatedProducts(categoryData.products, productId));
         }
-      } catch (error) {
-        setError(true);
-        setErrorMessage("Error en la carga");
-      } finally {
-        setLoading(false);
       }
-    };
+    }
+  }, [products, productId, category]);
 
-    fetchProduct();
-  }, [productId, category]); // Solo depende de productId y category
-
-  const getRelatedProducts = (products: any) => {
-    return products.filter((prod: any) => prod.id !== productId).slice(0, 3);
+  const getRelatedProducts = (products: any[], currentProductId: number) => {
+    return products.filter((prod: any) => prod.id !== currentProductId).slice(0, 3);
   };
 
   return (
@@ -82,45 +65,45 @@ export function PDP() {
         </section>
         <section className="product-characteristics">
           <h1>Características del producto</h1>
-          {loading ? (
+          {isLoading ? (
             <img src={spinner} alt="Cargando productos relacionados..." />
-          ) : error ? (
-            <Error message={errorMessage} />
+          ) : isError ? (
+            <Error message={error.message} />
           ) : (
-          <table>
-            <tbody>
-              {Object.entries(product || {}).map(([key, value]) => {
-                if (
-                  ![
-                    "id",
-                    "image",
-                    "originalPrice",
-                    "discountedPrice",
-                    "discountPercentage",
-                  ].includes(key)
-                ) {
-                  const formattedKey = key.replace(/([A-Z])/g, " $1").trim();
-                  return (
-                    <tr key={key}>
-                      <td>
-                        {formattedKey.charAt(0).toUpperCase() + formattedKey.slice(1)}
-                      </td>
-                      <td>{value as React.ReactNode}</td>
-                    </tr>
-                  );
-                }
-                return null;
-              })}
-            </tbody>
-          </table>
+            <table>
+              <tbody>
+                {Object.entries(product || {}).map(([key, value]) => {
+                  if (
+                    ![
+                      "id",
+                      "image",
+                      "originalPrice",
+                      "discountedPrice",
+                      "discountPercentage",
+                    ].includes(key)
+                  ) {
+                    const formattedKey = key.replace(/([A-Z])/g, " $1").trim();
+                    return (
+                      <tr key={key}>
+                        <td>
+                          {formattedKey.charAt(0).toUpperCase() + formattedKey.slice(1)}
+                        </td>
+                        <td>{value as React.ReactNode}</td>
+                      </tr>
+                    );
+                  }
+                  return null;
+                })}
+              </tbody>
+            </table>
           )}
         </section>
         <h1>Productos relacionados</h1>
         <section className="related-items">
-          {loading ? (
+          {isLoading ? (
             <img src={spinner} alt="Cargando productos relacionados..." />
-          ) : error ? (
-            <Error message={errorMessage} />
+          ) : isError ? (
+            <Error message={error.message} />
           ) : (
             relatedProducts.map((prod: any) => (
               <ProductCard
